2c839f02671321c0c83c0f8c92da5f92046181bc
harish <harishbabubalaji@gmail.com>
2021-07-17 12:28:22 +0530

    commit

--- main.py
+++ main.py
@@ -3,30 +3,23 @@ from pi_day_module import PiDay
 
 user_choice = int(input("Enter 1 for pi value and 2 for relation between e and pi, 3 for value of pi at a decimal place \n: "))
 
-while(1):
-  if user_choice == 1:
-    p = int(input("Enter a number of decimal places : "))
-    p = PiDay(p)
-    p.get_pi()
-    break
+if user_choice == 1:
+  p = int(input("Enter a number of decimal places : "))
+  p = PiDay(p)
+  p.get_pi()
 
-  elif user_choice == 2:
-    p = PiDay(1000)
+elif user_choice == 2:
+  p = PiDay(1000)
 
-    # geting the value of pi and e
-    p.get_pi()
-    print("\n")
-    p.compute_e()
-    p.get_relation_e()
+  # geting the value of pi and e
+  p.get_pi()
+  print("\n")
+  p.compute_e()
+  p.get_relation_e()
 
-    break
-
-  elif user_choice == 3:
-    p = int(input("Enter a number of decimal places : "))
-    p = PiDay(p)
-    p.value_at_place_pi()
-    break
-
-  else:
-    print("please enter a proper value")
-    break
+elif user_choice == 3:
+  p = int(input("Enter a number of decimal places : "))
+  p = PiDay(p)
+  p.value_at_place_pi()
+else:
+  print("please enter a proper value")
